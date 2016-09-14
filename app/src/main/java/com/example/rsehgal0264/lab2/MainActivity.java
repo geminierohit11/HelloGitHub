6d494903e8112546c3657a5fb888cffcd00726ed
Rohit Sehgal <Rsehgal0264@conestogac.on.ca>
2016-09-14 12:28:42 -0400

    Initial commit

---
+++ app/src/main/java/com/example/rsehgal0264/lab2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rsehgal0264.lab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
